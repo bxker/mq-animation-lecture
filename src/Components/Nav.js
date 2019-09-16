a4486c6da3aaa160843589f8997c7d64e6481107
bxker <jake.luke.baker@gmail.com>
2019-09-16 11:43:55 -0500

    init commit

---
+++ src/Components/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Nav.css'
+
+export default function Nav(){
+    return(
+        // <nav className="nav-container">
+        //     <div className="container-1">
+        //         <h1>Start Bootstrap</h1>
+        //     </div>
+        //     <div className="container-2">
+        //         <ul>
+        //             <li>Home</li>
+        //             <li>About</li>
+        //             <li>Services</li>
+        //             <li>Contact</li>
+        //             <li id="hamburger"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png" alt="hamburger"/></li>
+        //         </ul>
+        //     </div>
+        // </nav>
+        <div class="box-1">
+
+        </div>
+    )
+}
